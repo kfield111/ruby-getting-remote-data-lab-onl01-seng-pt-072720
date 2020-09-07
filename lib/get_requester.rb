5b0a30ec35e4a57bbe28d3a932dfd6f3ed5df67b
Kelin Field <kfield111@hotmail.com>
2020-09-07 01:18:36 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,18 @@
-# Write your code here
+require 'nte/https'
+require 'open-uri'
+require 'json'
+
+
+class GetRequester
+
+  def initialize (url_address)
+    @url_address = url_address
+  end
+
+  def get_response_body
+    uri = url_address
+    response = NET::HTTPS.get_response(uri)
+    response.body
+  end
+
+end
